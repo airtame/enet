5158beed0d7eaee07c2a7a0fc6212050c96eeaa2
Razvan Lar <raz@airtame.com>
2018-01-12 13:37:14 +0200

    Use proper stdint types
    Enet defined its own fixed width integers. These were incorrect, as they
    did not depend on the target platform. So just use stdint.
    
    Change-Id: Ibb0ad1ae4fcd9bd091ebc65fb9d0c587cb30cef1

--- include/enet/types.h
+++ include/enet/types.h
@@ -5,9 +5,14 @@
 #ifndef __ENET_TYPES_H__
 #define __ENET_TYPES_H__
 
-typedef unsigned char enet_uint8;       /**< unsigned 8-bit type  */
-typedef unsigned short enet_uint16;     /**< unsigned 16-bit type */
-typedef unsigned int enet_uint32;      /**< unsigned 32-bit type */
+#include <stdint.h>
+
+/* For some reason enet defined its own fixed size ints.
+   Use instead the standard ones, but keep the enet typedefs
+   so we don't need to change all the code. */
+typedef uint8_t enet_uint8;       /**< unsigned 8-bit type  */
+typedef uint16_t enet_uint16;     /**< unsigned 16-bit type */
+typedef uint32_t enet_uint32;     /**< unsigned 32-bit type */
 
 #endif /* __ENET_TYPES_H__ */
 
